d99048de43612e447f60fad7adbbb196ec6bb398
Polonez555 <antocie567@gmail.com>
2025-09-02 15:25:02 +0200

    db

--- src/main/java/ch/bzz/LibraryAppMain.java
+++ src/main/java/ch/bzz/LibraryAppMain.java
@@ -1,15 +1,45 @@
 package ch.bzz;
 
+import java.sql.*;
 import java.util.*;
 
 public class LibraryAppMain {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         Scanner scanner = new Scanner(System.in);
         Map<String, Runnable> commands = new HashMap<>();
+
+        Connection c = DriverManager.getConnection(
+                "jdbc:postgresql://localhost:5432/local_db", "postgres", "182007"
+        );
+
 
         commands.put("help", () -> {
             System.out.println("Verfügbare Befehle:");
             commands.keySet().forEach(System.out::println);
+        });
+
+        commands.put("list", () -> {
+            try {
+                Statement statement = c.createStatement();
+                ResultSet rs = null;
+
+                rs = statement.executeQuery("SELECT id, isbn, title, author, publication_year FROM books");
+
+
+                while (rs.next()) {
+                    int id = rs.getInt("id");
+                    String isbn = rs.getString("isbn");
+                    String title = rs.getString("title");
+                    String author = rs.getString("author");
+                    int year = rs.getInt("publication_year");
+
+                    System.out.printf("%d | %s | %s | %s | %d%n", id, isbn, title, author, year);
+                }
+                rs.close();
+                statement.close();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
         });
 
         commands.put("quit", () -> {
